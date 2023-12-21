924130bfff06e4961033c65087aef214644cc645
Kavin <78342682+kavin0411@users.noreply.github.com>
2023-12-21 18:11:09 +0530

    Added sending notifiaction for multiple roles on workflow

--- telegram_productivity/api.py
+++ telegram_productivity/api.py
@@ -8,16 +8,23 @@ def send_telegram_msg(self, method):
 		if self.approval_status == 'Draft':
 			employee_id = self.employee
 			expense_approver = self.expense_approver
-			from_user = frappe.db.get_value("Telegram Employee ID", {"employee_id":employee_id}, ["telegram_username"])
-			user_id, username = frappe.db.get_value("Telegram Employee ID", {"company_email":expense_approver}, ["telegram_user_id", "telegram_username"])
+			from_user = frappe.db.get_value("Telegram User Details", {"user":employee_id}, ["telegram_username"])
+			user_id, username = frappe.db.get_value("Telegram User Details", {"user":expense_approver}, ["telegram_user_id", "telegram_username"])
 			doc_name =self.name
-			try:
-				asyncio.run(send_notification(user_id=user_id,username=username,doc_name=doc_name,from_user = from_user))
-				frappe.msgprint("telegram notification sent successfully")
-			except Exception as e:
-				frappe.msgprint("Error: " + str(e))
-				# else:
-				# 	frappe.db.commit()
+			asyncio.run(send_notification(user_id=user_id,username=username,doc_name=doc_name,status = self.approval_status,from_user = from_user))
+			frappe.msgprint("telegram notification sent successfully")
+		elif self.approval_status == 'Approved':
+			user_id = frappe.db.get_value("Employee",filters = {"name":self.employee},fieldname = "user_id")
+			from_user = self.expense_approver
+			hr_names = frappe.db.get_list("User",filters= {"role_profile_name" : "HR Manager"}, fields = ["name"])
+			for name in hr_names:
+				user_id, username = frappe.db.get_value("Telegram User Details", {"user":name.get('name')}, ["telegram_user_id", "telegram_username"])
+				doc_name =self.name
+				loop = asyncio.get_event_loop()
+				task = loop.create_task(send_notification(user_id=user_id,username=username,doc_name=doc_name,status = self.approval_status, from_user = from_user))
+				loop.run_until_complete(task)
+				#asyncio.run(send_notification(user_id=user_id,username=username,doc_name=doc_name,status = self.approval_status, from_user = from_user))
+				frappe.msgprint("telegram notification sent to HR successfully")
 		else:
 			frappe.log_error(str(e))
 	except Exception as e:
