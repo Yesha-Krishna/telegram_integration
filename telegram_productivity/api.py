e2c317100ed4f542b0186b23a4b46a289c12f17e
Sugesh393 <sugeshgk002@gmail.com>
2024-07-01 16:19:30 +0530

    Added name in message

--- telegram_productivity/api.py
+++ telegram_productivity/api.py
@@ -6,9 +6,9 @@ from telegram_productivity.telegram_productivity.utils.telegram_bot import send_
 def send_telegram_msg(self, method):
 	try:
 		if self.approval_status == 'Draft':
-			employee_id = self.employee
+			# employee_id = self.employee
 			expense_approver = self.expense_approver
-			from_user = frappe.db.get_value("Telegram User Details", {"user":employee_id}, ["telegram_username"])
+			from_user = self.employee_name # frappe.db.get_value("Telegram User Details", {"user":employee_id}, ["telegram_username"])
 			user_id, username = frappe.db.get_value("Telegram User Details", {"user":expense_approver}, ["telegram_user_id", "telegram_username"])
 			doc_name =self.name
 			asyncio.run(send_notification(user_id=user_id,username=username,doc_name=doc_name,status = self.approval_status,from_user = from_user))
